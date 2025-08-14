7ba63fe7ccb7a448e0a731bd2bfaa1558031b2ae
harithh07 <harith.hakim2002@gmail.com>
2025-08-14 15:46:07 +0800

    Add output of enriched logs into a JSON file

--- logparser.py
+++ logparser.py
@@ -1,10 +1,12 @@
 import sys
 import re
 from user_agents import parse
+import json
 
 def parse_log(file):
     pattern = re.compile(r'(?P<ip>\S+) - - \[(?P<time>.*?)] "(?P<method>.*?) (?P<url>.*?) (?P<protocol>.*?)"'
                          r' (?P<status>\d+) (?P<size>\d+) "(?P<referrer>.*?)" "(?P<user_agent>.*?)"')
+    logs = []
 
     with open(file, 'r') as f:
         for line in f:
@@ -18,7 +20,9 @@ def parse_log(file):
                     data['device'] = user_agent.device.family
                 else:
                     data['device'] = "Unknown"
-                print(data)
+                logs.append(data)
+
+    return logs
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -26,5 +30,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     log_file = sys.argv[1]
-    parse_log(log_file)
+
+    enriched_logs = parse_log(log_file)
+    output_file = "output_logs.json"
+
+    with open(output_file, 'w') as f:
+        json.dump(enriched_logs, f, indent=4)
 
